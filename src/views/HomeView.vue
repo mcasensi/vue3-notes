dee8b3745b7c2401cf9083a559a69fba35dd98ee
Marj <marj_444_bluepink@yahoo.com>
2022-07-22 10:10:34 +0800

    add and remove

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,43 +1,40 @@
 <template>
     <div>
-        <div ref="eventsDiv" class="eventsJSON">
-            <div v-for="(event, index) in eventLog" :key="index">
-                {{ event }}
+        <div class="layoutJSON">
+            Displayed as <code>[x, y, w, h]</code>:
+            <div class="columns">
+                <div class="layoutItem" v-for="item in layout" :key="item.i">
+                    <b>{{ item.i }}</b
+                    >: [{{ item.x }}, {{ item.y }}, {{ item.w }}, {{ item.h }}]
+                </div>
             </div>
         </div>
-        <div style="margin-top: 10px">
-            <grid-layout
-                :layout="layout"
-                :col-num="12"
-                :row-height="30"
-                :is-draggable="draggable"
-                :is-resizable="resizable"
-                :vertical-compact="true"
-                :use-css-transforms="true"
-                @layout-created="layoutCreatedEvent"
-                @layout-before-mount="layoutBeforeMountEvent"
-                @layout-mounted="layoutMountedEvent"
-                @layout-ready="layoutReadyEvent"
-                @layout-updated="layoutUpdatedEvent"
+        <button @click="addItem">Add an item dynamically</button>
+        <input type="checkbox" v-model="draggable" /> Draggable
+        <input type="checkbox" v-model="resizable" /> Resizable
+        <grid-layout
+            :layout="layout"
+            :col-num="colNum"
+            :row-height="30"
+            :is-draggable="draggable"
+            :is-resizable="resizable"
+            :vertical-compact="true"
+            :use-css-transforms="true"
+        >
+            <grid-item
+                v-for="(item, index) in layout"
+                :key="index"
+                :static="item.static"
+                :x="item.x"
+                :y="item.y"
+                :w="item.w"
+                :h="item.h"
+                :i="item.i"
             >
-                <grid-item
-                    v-for="(item, index) in layout"
-                    :key="index"
-                    :x="item.x"
-                    :y="item.y"
-                    :w="item.w"
-                    :h="item.h"
-                    :i="item.i"
-                    @resize="resizeEvent"
-                    @move="moveEvent"
-                    @resized="resizedEvent"
-                    @container-resized="containerResizedEvent"
-                    @moved="movedEvent"
-                >
-                    <span class="text">{{ item.i }}</span>
-                </grid-item>
-            </grid-layout>
-        </div>
+                <span class="text">{{ item.i }}</span>
+                <span class="remove" @click="removeItem(item.i)">x</span>
+            </grid-item>
+        </grid-layout>
     </div>
 </template>
 
@@ -51,132 +48,74 @@ export default {
     data() {
         return {
             layout: [
-                { x: 0, y: 0, w: 2, h: 2, i: "0", static: false },
-                { x: 2, y: 0, w: 2, h: 4, i: "1", static: true },
-                { x: 4, y: 0, w: 2, h: 5, i: "2", static: false },
-                { x: 6, y: 0, w: 2, h: 3, i: "3", static: false },
-                { x: 8, y: 0, w: 2, h: 3, i: "4", static: false },
-                { x: 10, y: 0, w: 2, h: 3, i: "5", static: false },
-                { x: 0, y: 5, w: 2, h: 5, i: "6", static: false },
-                { x: 2, y: 5, w: 2, h: 5, i: "7", static: false },
-                { x: 4, y: 5, w: 2, h: 5, i: "8", static: false },
-                { x: 6, y: 3, w: 2, h: 4, i: "9", static: true },
-                { x: 8, y: 4, w: 2, h: 4, i: "10", static: false },
-                { x: 10, y: 4, w: 2, h: 4, i: "11", static: false },
-                { x: 0, y: 10, w: 2, h: 5, i: "12", static: false },
-                { x: 2, y: 10, w: 2, h: 5, i: "13", static: false },
-                { x: 4, y: 8, w: 2, h: 4, i: "14", static: false },
-                { x: 6, y: 8, w: 2, h: 4, i: "15", static: false },
-                { x: 8, y: 10, w: 2, h: 5, i: "16", static: false },
-                { x: 10, y: 4, w: 2, h: 2, i: "17", static: false },
-                { x: 0, y: 9, w: 2, h: 3, i: "18", static: false },
-                { x: 2, y: 6, w: 2, h: 2, i: "19", static: false },
+                { x: 0, y: 0, w: 2, h: 2, i: "0" },
+                { x: 2, y: 0, w: 2, h: 2, i: "1" },
+                { x: 4, y: 0, w: 2, h: 2, i: "2" },
+                { x: 6, y: 0, w: 2, h: 2, i: "3" },
+                { x: 8, y: 0, w: 2, h: 2, i: "4" },
             ],
             draggable: true,
             resizable: true,
+            colNum: 12,
             index: 0,
-            eventLog: [],
         };
     },
-    watch: {
-        eventLog: function () {
-            const eventsDiv = this.$refs.eventsDiv;
-            eventsDiv.scrollTop = eventsDiv.scrollHeight;
-        },
+    mounted() {
+        // this.$gridlayout.load();
+        this.index = this.layout.length;
     },
     methods: {
-        moveEvent: function (i, newX, newY) {
-            const msg = "MOVE i=" + i + ", X=" + newX + ", Y=" + newY;
-            this.eventLog.push(msg);
-            console.log(msg);
+        addItem: function () {
+            // Add a new item. It must have a unique key!
+            this.layout.push({
+                x: (this.layout.length * 2) % (this.colNum || 12),
+                y: this.layout.length + (this.colNum || 12), // puts it at the bottom
+                w: 2,
+                h: 2,
+                i: this.index,
+            });
+            // Increment the counter to ensure key is always unique.
+            this.index++;
         },
-        movedEvent: function (i, newX, newY) {
-            const msg = "MOVED i=" + i + ", X=" + newX + ", Y=" + newY;
-            this.eventLog.push(msg);
-            console.log(msg);
-        },
-        resizeEvent: function (i, newH, newW, newHPx, newWPx) {
-            const msg =
-                "RESIZE i=" +
-                i +
-                ", H=" +
-                newH +
-                ", W=" +
-                newW +
-                ", H(px)=" +
-                newHPx +
-                ", W(px)=" +
-                newWPx;
-            this.eventLog.push(msg);
-            console.log(msg);
-        },
-        resizedEvent: function (i, newX, newY, newHPx, newWPx) {
-            const msg =
-                "RESIZED i=" +
-                i +
-                ", X=" +
-                newX +
-                ", Y=" +
-                newY +
-                ", H(px)=" +
-                newHPx +
-                ", W(px)=" +
-                newWPx;
-            this.eventLog.push(msg);
-            console.log(msg);
-        },
-        containerResizedEvent: function (i, newH, newW, newHPx, newWPx) {
-            const msg =
-                "CONTAINER RESIZED i=" +
-                i +
-                ", H=" +
-                newH +
-                ", W=" +
-                newW +
-                ", H(px)=" +
-                newHPx +
-                ", W(px)=" +
-                newWPx;
-            this.eventLog.push(msg);
-            console.log(msg);
-        },
-        layoutCreatedEvent: function (newLayout) {
-            this.eventLog.push("Created layout");
-            console.log("Created layout: ", newLayout);
-        },
-        layoutBeforeMountEvent: function (newLayout) {
-            this.eventLog.push("beforeMount layout");
-            console.log("beforeMount layout: ", newLayout);
-        },
-        layoutMountedEvent: function (newLayout) {
-            this.eventLog.push("Mounted layout");
-            console.log("Mounted layout: ", newLayout);
-        },
-        layoutReadyEvent: function (newLayout) {
-            this.eventLog.push("Ready layout");
-            console.log("Ready layout: ", newLayout);
-        },
-        layoutUpdatedEvent: function (newLayout) {
-            this.eventLog.push("Updated layout");
-            console.log("Updated layout: ", newLayout);
+        removeItem: function (val) {
+            const index = this.layout.map((item) => item.i).indexOf(val);
+            this.layout.splice(index, 1);
         },
     },
 };
 </script>
 
-<style scoped>
+<style>
+.layoutJSON {
+    background: rgb(218, 179, 255);
+    border: 1px solid black;
+    margin-top: 10px;
+    padding: 10px;
+}
+.columns {
+    -moz-columns: 120px;
+    -webkit-columns: 120px;
+    columns: 120px;
+}
+/*************************************/
+.remove {
+    position: absolute;
+    right: 2px;
+    top: 0;
+    cursor: pointer;
+}
 .vue-grid-layout {
-    background: #eee;
+    background: rgb(255, 255, 255);
 }
 .vue-grid-item:not(.vue-grid-placeholder) {
-    background: #ccc;
+    background: rgb(222, 173, 255);
     border: 1px solid black;
 }
 .vue-grid-item .resizing {
     opacity: 0.9;
 }
 .vue-grid-item .static {
-    background: #cce;
+    background: rgb(192, 192, 255);
 }
 .vue-grid-item .text {
     font-size: 24px;
@@ -214,19 +153,5 @@ export default {
     background-origin: content-box;
     box-sizing: border-box;
     cursor: pointer;
-}
-.layoutJSON {
-    background: #ddd;
-    border: 1px solid black;
-    margin-top: 10px;
-    padding: 10px;
-}
-.eventsJSON {
-    background: #ddd;
-    border: 1px solid black;
-    margin-top: 10px;
-    padding: 10px;
-    height: 100px;
-    overflow-y: scroll;
 }
 </style>
